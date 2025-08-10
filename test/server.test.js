6003f32c08ce85a2f6ec88632db3432f151b4289
fritz-net <2499282+fritz-net@users.noreply.github.com>
2025-08-10 11:54:43 +0200

    test: added tests for dir changes

---
+++ test/server.test.js
@@ -0,0 +1,30 @@
+// @ts-check
+const request = require('supertest');
+const path = require('path');
+
+describe('Server configuration', () => {
+  let server;
+  beforeAll(() => {
+    process.env.CIVBUILDER_HOSTNAME = 'http://testhost/civbuilder';
+    server = require('../server.js');
+  });
+
+  afterAll(() => {
+    jest.resetModules();
+  });
+
+  test('should serve /civbuilder routes', async () => {
+    const res = await request(server.router).get('/');
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('should use configurable hostname', () => {
+    expect(process.env.CIVBUILDER_HOSTNAME).toBe('http://testhost/civbuilder');
+  });
+
+  test('should use temp dir for file operations', () => {
+    const os = require('os');
+    const expectedDir = path.join(os.tmpdir(), 'civbuilder');
+    expect(server.dir || '').toContain(expectedDir);
+  });
+});
